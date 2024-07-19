884f34cdde5d0bffb4a991fe35c7d9db2b0db4af
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-19 21:11:52 +0530

    Update Java Strings Introduction.java

--- Java Strings Introduction.java
+++ Java Strings Introduction.java
@@ -6,14 +6,19 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        
+        String A = sc.nextLine().trim();
+        String B = sc.nextLine().trim();
+        int lengthSum = A.length()+B.length();
+        System.out.println(lengthSum);
+        if (A.compareTo(B)>0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        String capitalizedA = A.substring(0,1).toUpperCase()+A.substring(1);
+        String capitalizedB = B.substring(0,1).toUpperCase()+B.substring(1);
+        System.out.println(capitalizedA+" "+capitalizedB);
+        sc.close();
     }
 }
-
-
-
