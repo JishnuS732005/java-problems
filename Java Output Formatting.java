159b11f7e12b9d5f4bf97d5f51e28a32d06818c7
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-19 20:58:03 +0530

    Update Java Output Formatting.java

--- Java Output Formatting.java
+++ Java Output Formatting.java
@@ -10,12 +10,12 @@ public class Solution {
             for(int i=0;i<3;i++){
                 String s1=sc.next();
                 int x=sc.nextInt();
-                //Complete this line
+                System.out.printf("%-15s%03d%n",s1,x);
             }
             System.out.println("================================");
+            sc.close();
 
     }
 }
-
 
 
