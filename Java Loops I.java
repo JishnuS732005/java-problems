091978879333b77389e890e502eba1cf617dacc6
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-19 20:12:26 +0530

    Create Java Loops I.java

---
+++ Java Loops I.java
@@ -0,0 +1,21 @@
+#  https://www.hackerrank.com/challenges/java-loops-i/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+    }
+}
