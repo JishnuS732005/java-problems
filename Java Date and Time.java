7c3a84a5b71e5739d20a8f3b11a7e6a7375e2940
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-19 21:10:35 +0530

    Update Java Date and Time.java

--- Java Date and Time.java
+++ Java Date and Time.java
@@ -1,50 +1,26 @@
 #  https://www.hackerrank.com/challenges/java-date-and-time/
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.util.Scanner;
 
 class Result {
-
-    /*
-     * Complete the 'findDay' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER month
-     *  2. INTEGER day
-     *  3. INTEGER year
-     */
-
     public static String findDay(int month, int day, int year) {
-
+        LocalDate date=LocalDate.of(year,month,day);
+        return date.getDayOfWeek().getDisplayName(TextStyle.FULL,Locale.ENGLISH).toUpperCase();
     }
-
 }
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int month = Integer.parseInt(firstMultipleInput[0]);
-
-        int day = Integer.parseInt(firstMultipleInput[1]);
-
-        int year = Integer.parseInt(firstMultipleInput[2]);
-
-        String res = Result.findDay(month, day, year);
-
-        bufferedWriter.write(res);
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        Scanner sc=new Scanner(System.in);
+        int month=sc.nextInt();
+        int day=sc.nextInt();
+        int year=sc.nextInt();
+        
+        String res=Result.findDay(month,day,year);
+        System.out.println(res);
     }
 }
