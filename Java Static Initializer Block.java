6729587951b91d662a173ffed740a5eaf88e0884
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-19 21:08:16 +0530

    Update Java Static Initializer Block.java

--- Java Static Initializer Block.java
+++ Java Static Initializer Block.java
@@ -2,21 +2,19 @@
 
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
-Write your code here
-
-public static void main(String[] args){
-		if(flag){
-			int area=B*H;
-			System.out.print(area);
-		}
-		
-	}//end of main
-
-}//end of class
-
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int B = sc.nextInt();
+        int H = sc.nextInt();
+        if(B>0 && H>0) {
+            int area=B*H;
+            System.out.println(area);
+        }
+        else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+}
