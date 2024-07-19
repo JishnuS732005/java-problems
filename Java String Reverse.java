a40ab83c2e6a4321c90751df58aa4a53843f234f
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-19 21:13:43 +0530

    Update Java String Reverse.java

--- Java String Reverse.java
+++ Java String Reverse.java
@@ -6,13 +6,16 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        
+        String s=sc.nextLine();
+        if (isPalindrome(s)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        sc.close();
+    }
+    private static boolean isPalindrome(String s) {
+        return s.equals(new StringBuilder(s).reverse().toString());
     }
 }
-
-
-
