355b7cfc319c14d6be88adcdcd4e543652b04581
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-19 21:07:20 +0530

    Update Java Datatypes.java

--- Java Datatypes.java
+++ Java Datatypes.java
@@ -1,15 +1,12 @@
 # https://www.hackerrank.com/challenges/java-datatypes/
 
+
 import java.util.*;
 import java.io.*;
-
-
 
 class Solution{
     public static void main(String []argh)
     {
-
-
 
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
@@ -21,8 +18,18 @@ class Solution{
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                //Complete the code
+                if(x>=-128 && x<=127){
+                    System.out.println("* byte");
+                }
+                if(x>=-32768 && x<=32767) {
+                    System.out.println("* short");
+                }
+                if(x>=-2147483648L && x<=2147483647L) {
+                    System.out.println("* int");
+                }
+                if(x>=-9223372036854775808L && x<=9223372036854775807L){
+                    System.out.println("* long");
+                }
             }
             catch(Exception e)
             {
@@ -30,8 +37,6 @@ class Solution{
             }
 
         }
+        sc.close();
     }
 }
-
-
-
